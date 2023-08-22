417759f49faa9cd90cd213bbc2d51cd5743166c4
eduardomarchiori <eduardo@voyagerportal.com>
2023-08-22 09:57:49 -0300

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtLayout>
+    <div class="error-content flex flex-col justify-center items-center">
+      <p class="text-7xl front-bold">{{ error.statusCode }}</p>
+      <img
+        src="https://cdn.svgator.com/images/2022/01/funny-404-error-page-design.gif"
+        alt="animation"
+        width="400"
+      />
+      <p>{{ error.message }}</p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+defineProps(["error"]);
+</script>
+
+<style scoped>
+img {
+  margin: -12px 0;
+}
+
+p {
+  z-index: 1;
+}
+
+.error-content {
+  flex-grow: 1;
+}
+
+.error-content {
+  flex-shrink: 0;
+}
+</style>
